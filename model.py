9304cc3201f2179b262080b6ba761dab76c19c81
Ritwik <ritwiktiwari24@gmail.com>
2018-07-22 21:31:33 +0530

    backend complete

--- model.py
+++ model.py
@@ -5,12 +5,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
 data = pd.read_csv('dataset.csv')
-X = data[['Dependents', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History', 'Property_Area', 'Gender_Male', 'Married_Yes', 'Education_Not Graduate', 'Self_Employed_Yes']]
+X = data[['Gender_Male', 'Married_Yes', 'Education_Not Graduate', 'Self_Employed_Yes', 'Dependents', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Loan_Amount_Term', 'Credit_History', 'Property_Area', ]]
 y = data['Loan_Status']
 sc_X = StandardScaler()
 sc_X.fit(X)
 X = sc_X.transform(X)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-randomforrestclassifier = RandomForestClassifier(n_estimators=61)
-randomforrestclassifier.fit(X_train, y_train)
-print(randomforrestclassifier.score(X_test, y_test))
+# randomforrestclassifier = RandomForestClassifier(n_estimators=61)
+# randomforrestclassifier.fit(X, y)
+# print(randomforrestclassifier.score(X_test, y_test))
+from sklearn.linear_model import LogisticRegression
+logistic = LogisticRegression()
+logistic.fit(X,y)
